3b9d2063f2c0e4c8d7fa5693ba5c10ed9f43f57d
valov <valov.eu@yandex.ru>
2021-10-21 21:01:28 +0300

    Домашка

--- webapp/note/admin.py
+++ webapp/note/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Note
 
-admin.site.register(Note)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'massage', 'date_add', 'public')
+ 
+
+admin.site.register(Note, NoteAdmin)
